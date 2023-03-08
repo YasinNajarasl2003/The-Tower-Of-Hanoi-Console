79d881c8f3c26cb5dd1df561735ab81972195e12
YasinNajarasl <yasinnajarasl@gmail.com>
2023-03-08 21:41:10 +0330

    Add project files.

---
+++ TowerOfHanoi/Calculate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerOfHanoi
+{
+    public class Calculate
+    {
+        private static long Moves = 0;
+        public static void Towers(int number, int from, int to, int free)
+        {
+            if (number == 0)
+                return;
+            Towers(number - 1, from, free, to);
+            Console.WriteLine($"{++Moves}_from: {from} To: {to}");
+            Towers(number - 1, free, to, from);
+        }
+    }
+}
---
+++ TowerOfHanoi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using TowerOfHanoi;
+bool flag = true;
+while(flag)
+{
+    int diskCount = 0;
+    Console.Write("how many disk do you want to calculate (Zero to exit):");
+    try
+    {
+        diskCount = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        break;
+    }
+    if(diskCount == 0)
+    {
+        flag = false;
+        break;
+    }
+
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    Calculate.Towers(diskCount, 1, 3, 2);
+    stopwatch.Stop();
+    Console.WriteLine(stopwatch.ElapsedMilliseconds);
+}
